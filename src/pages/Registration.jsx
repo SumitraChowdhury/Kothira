9d6b8c7eed861b8f56c13ecf5eae3ef02bb78ec5
SumitraChowdhury <sumitrachow071@gmail.com>
2026-09-22 18:48:58 +0600

    registration finish

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -11,11 +11,11 @@ const Registration = () => {
         <div className='flex justify-end items-center h-full'>
           <div className="w-[570px]">
             <h2 className='text-34 text-primary font-bold font-nunito'>Get started with easily register</h2>
-            <p className='text-lg text-black/50 font-normal font-nunito'>Free register and you can enjoy it</p>
-            <TextField id="outlined-basic" label="Email Address" variant="outlined" />
-            <TextField id="outlined-basic" label="Ful name" variant="outlined" />
-            <TextField id="outlined-basic" label="Password" variant="outlined" />
-            <Button variant="contained">Sign up</Button>
+            <p className='text-lg text-black/50 font-normal font-nunito pt-3'>Free register and you can enjoy it</p>
+            <TextField className=' w-[80%] mt-5!' id="outlined-basic" label="Email Address" variant="outlined" />
+            <TextField className=' w-[80%] mt-5!' id="outlined-basic" label="Ful name" variant="outlined" />
+            <TextField className=' w-[80%] mt-5!' id="outlined-basic" label="Password" variant="outlined" />
+            <Button className='bg-[#5F35F5]! w-[70%] mt-10! mb-5! rounded-full!' variant="contained">Sign up</Button>
             <p className='text-14 text-secondary font-opensans'>Already have an account ? <span className='text-third'>Sign In</span></p>
           </div>
         </div>
